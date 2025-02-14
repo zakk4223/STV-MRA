dac7264a7ac6a30692d945d370f67a90f23f8019
Zakk <zakk@rsdio.com>
2025-02-13 21:31:04 -0500

    Change generation script so it properly byte skips certain roms.

--- script/gen_mra.py
+++ script/gen_mra.py
@@ -18,16 +18,21 @@ hashtree = ET.parse(mame_stvhash_file)
 hashroot = hashtree.getroot()
 
 
-def add_rom_part(romroot, crc, name, byteswap=False):
+def add_rom_part(romroot, crc, name, length=None, byteswap=False, skip_byte=False):
     inter_elem = ET.SubElement(romroot, "interleave", output="64")
-    ET.SubElement(inter_elem, "part", crc=crc, name=name, map="21436587" if byteswap else "12345678")
+    byte_map = "21436587" if byteswap else "12345678"
+    if skip_byte:
+        byte_map = "01030507" if byteswap else "02040608"
+    ET.SubElement(inter_elem, "part", crc=crc, name=name, map=byte_map)
 
 def add_rom(mraroot, romindex, zipfiles, address):
     rom_elem = ET.SubElement(mraroot, "rom", index=romindex, md5="", zip="|".join(zipfiles), address=address)
     return rom_elem
 
 def add_zero_bytes(romroot, length):
-    ET.SubElement(romroot, "part", repeat=hex(length)).text = "00"
+    zelem = ET.SubElement(romroot, "part", repeat=hex(length))
+    zelem.text = "00"
+    return zelem
 
 def add_bios(mraroot, region="US"):
     bios_elem = add_rom(mraroot, romindex="2", zipfiles=["stvbios.zip"], address="0x30000000")
@@ -59,22 +64,36 @@ def create_mra_tree(gameinfo, for_region="US"):
     last_loaded_rom_node = None
     current_offset = 0
     for rominfo in gameinfo.iter('rom'):
+        #HEY YOU IN THE FUTURE:
+        #_almost_ every game that has load16_byte flags also has an offset like x000001
+        #this means it writes every other byte into the region, but starts at byte 1
+        #00 xx before byte/word etc swap.
+        #a few games are more complicated than this, since you can use this to interleave two roms
+        #via offsets of 0x000000 and then 0x000001
+        #it might be worth detecting this at some point but for now just set a flag and then 2x the size
+        #since we'll be writing 00xx
+        #notable game that might need hand fixing: batman
+        if 'loadflag' in rominfo.attrib and rominfo.attrib['loadflag'] == 'reload' and last_loaded_rom_node is not None:
+            rominfo = last_loaded_rom_node
+            print(descnode.text)
+        rom_byte_skip = False
+        if 'loadflag' in rominfo.attrib and rominfo.attrib['loadflag'] == 'load16_byte': rom_byte_skip = True
         rom_offset = int(rominfo.attrib['offset'],0)
         rom_offset = rom_offset & ~1
         if rom_offset > current_offset:
             add_zero_bytes(rom_root, length=rom_offset - current_offset)
             current_offset = current_offset + (rom_offset - current_offset)
-        if 'loadflag' in rominfo.attrib and rominfo.attrib['loadflag'] == 'reload' and last_loaded_rom_node is not None:
-            rominfo = last_loaded_rom_node
-            print(descnode.text)
         if 'name' in rominfo.attrib:
             rom_size = int(rominfo.attrib['size'],0)
+            if rom_byte_skip: rom_size = rom_size * 2
             do_byteswap = True 
             if 'loadflag' in rominfo.attrib and rominfo.attrib['loadflag'] == 'load16_word_swap':
                 do_byteswap = False
+            if 'loadflag' in rominfo.attrib and rominfo.attrib['loadflag'] == 'load16_byte':
+                do_byteswap = False
             if current_offset < 0x200000:
                 do_byteswap = not do_byteswap
-            add_rom_part(rom_root, crc=rominfo.attrib['crc'], name=rominfo.attrib['name'], byteswap=do_byteswap)
+            add_rom_part(rom_root, length=rom_size, crc=rominfo.attrib['crc'], name=rominfo.attrib['name'], byteswap=do_byteswap, skip_byte=rom_byte_skip)
             last_loaded_rom_node = rominfo
             current_offset = current_offset + rom_size
 
